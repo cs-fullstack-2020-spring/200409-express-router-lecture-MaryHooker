748b8657344f28b156069e0c0cc506cbc43fd8f3
MaryHooker <mc020791@yahoo.com>
2020-04-09 09:12:29 -0500

    point of entry/listener

---
+++ index.js
@@ -0,0 +1,11 @@
+//20-04-09 Express Router Lecture
+
+//Create point of entry
+let express = require('express');
+let app = express();
+
+//Add listener
+app.listen(8000,()=>{
+    console.log('Listening on port 8000')
+})
+
